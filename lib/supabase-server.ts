3d1014b81555ada9b4541cc6e0c9d26580ab1044
Jagdish Lade <47141343+ispeedbiz@users.noreply.github.com>
2026-03-25 19:37:11 -0400

    feat: lib/supabase-server.ts

---
+++ lib/supabase-server.ts
@@ -0,0 +1,7 @@
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+
+export function getSupabaseServerClient() {
+  const cookieStore = cookies();
+  return createServerComponentClient({ cookies: () => cookieStore });
+}
